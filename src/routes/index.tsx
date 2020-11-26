f7112b986eb1bde3601d06a94b9b8b34478c0afc
Alexandre <av.vessoni@gmail.com>
2020-11-26 23:30:51 +0000

    Added Routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Dashboard from '../pages/Dashboards';
+import Repository from '../pages/Repository';
+
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={Dashboard} />
+    <Route path="/repository" exact component={Repository} />
+  </Switch>
+);
+
+export default Routes;
